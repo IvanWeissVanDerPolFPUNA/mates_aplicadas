7566d14c46153034ad79220a9bd917dbc955cb95
Jose Luis Junior Gutierrez Aguero <jlgutierrez2000@hotmail.com>
2022-08-26 16:03:12 -0400

    Minor changes

--- tp4/tp4.py
+++ tp4/tp4.py
@@ -1,30 +1,28 @@
 import decimal
-import re
 import numpy as np
-A = dict()
-B = dict()
-# X = [1,2,3,4,5]
-# Y = ["a","b","c","d","e","f"]
 
-# A = {"1":0.3,"2":0.5, "3":1,"4":0.7,"5":0.2 }
-# B = {"a":0.3,"b":0.5, "c":0.8,"d":1 }
+# Datos del ejercicio
+X = [1, 2, 3, 4, 5]
+Y = ["a", "b", "c", "d", "e", "f"]
+A = {"1": 0.3, "2": 0.5, "3": 1, "4": 0.7, "5": 0.2}
+B = {"a": 0.3, "b": 0.5, "c": 0.8, "d": 1}
 
+'''
 # ejercicio de ejemplo
 X = [1, 2, 3]
 Y = ["a", "b"]
 A = {"1": 0.1, "2": 0.4, "3": 1}
 B = {"a": 0.3, "b": 1}
-
+'''
 
 def Ejercico1(A_set, B_set):
     print("###################################")
-    print("# ejercicio 1                     #")
+    print("# Ejercicio 1                     #")
     print("###################################")
     A_len = len(A_set)
     B_len = len(B_set)
 
-    GODEL_mat = np.array(
-        [0]*A_len*B_len, dtype=np.dtype(decimal.Decimal)).reshape(A_len, B_len)
+    GODEL_mat = np.array([0]*A_len*B_len, dtype=np.dtype(decimal.Decimal)).reshape(A_len, B_len)
     LUKASIEWICZ_mat = np.array([0]*A_len*B_len).reshape(A_len, B_len)
     GOGUEN_mat = np.array([0]*A_len*B_len).reshape(A_len, B_len)
 
@@ -35,8 +33,7 @@ def Ejercico1(A_set, B_set):
             GOGUEN_value = goguen_op(A_set[A_key], B_set[B_key])
 
             GODEL_mat = set_value(GODEL_mat, A_pos, B_pos, GODEL_value)
-            LUKASIEWICZ_mat = set_value(
-                LUKASIEWICZ_mat, A_pos, B_pos, LUKASIEWICZ_value)
+            LUKASIEWICZ_mat = set_value(LUKASIEWICZ_mat, A_pos, B_pos, LUKASIEWICZ_value)
             GOGUEN_mat = set_value(GOGUEN_mat, A_pos, B_pos, GOGUEN_value)
 
             print("A_key = " + A_key + " A_Value = " + str(A_set[A_key]))
@@ -86,11 +83,11 @@ def set_value(mat, A_pos, B_pos, val):
 
 def Ejercicio2(A, B, GODEL_mat, LUKASIEWICZ_mat, GOGUEN_mat):
     print("###################################")
-    print("# ejercicio 2                     #")
+    print("# Ejercicio 2                     #")
     print("###################################")
-    #A_P = {"1":0.5, "2":0.7, "3":0.9, "4":0.5, "5":0.4 }
+    A_P = {"1": 0.5, "2": 0.7, "3": 0.9, "4": 0.5, "5": 0.4}
     # ejercicio de ejemplo
-    A_P = {"1": 0.5, "2": 1, "3": 0.9}
+    #A_P = {"1": 0.5, "2": 1, "3": 0.9}
 
     A_P_mat = np.array(list(A_P.values()))
 
@@ -131,8 +128,13 @@ def max_op(A_set, B_set, mat, arr):
 
 
 ########################
-#       main           #
+#         main         #
 ########################
 
-GODEL_mat, LUKASIEWICZ_mat, GOGUEN_mat = Ejercico1(A, B)
-Ejercicio2(A, B, GODEL_mat, LUKASIEWICZ_mat, GOGUEN_mat)
+def main():
+    GODEL_mat, LUKASIEWICZ_mat, GOGUEN_mat = Ejercico1(A, B)
+    Ejercicio2(A, B, GODEL_mat, LUKASIEWICZ_mat, GOGUEN_mat)
+
+
+if __name__ == "__main__":
+    main()
